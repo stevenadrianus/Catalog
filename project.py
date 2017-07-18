b1d82f68b958c91c217573f5aa989152279ce8a3
Steven Adrianus <stevenadrianus@Stevens-MacBook-Pro.local>
2017-07-17 21:29:20 -0700

    Finished all the features

--- project.py
+++ project.py
@@ -4,9 +4,8 @@ import httplib2
 import json
 import requests
 
-
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
-from flask import make_response
+from flask import Flask, render_template, request, redirect, jsonify, url_for
+from flask import make_response, flash
 from flask import session as login_session
 
 from sqlalchemy import create_engine, asc
@@ -193,7 +192,10 @@ def gdisconnect():
 def showMain():
     catalogs = session.query(Catalog.category).distinct()
     items = session.query(Catalog.id, Catalog.name, Catalog.category).distinct()
-    return render_template('catalog.html', categories=catalogs, items=items)
+    if 'username' not in login_session:
+        return render_template('catalog.html', categories=catalogs, items=items)
+    else:
+        return render_template('private_catalog.html', categories=catalogs, items=items)
 
 @app.route('/<cat>/')
 def showCategory(cat):
@@ -204,9 +206,74 @@ def showCategory(cat):
 @app.route('/<int:item_id>/')
 def showItem(item_id):
     item = session.query(Catalog).filter_by(id=item_id).one()
-    return render_template('public_item.html', item=item)
+    creator = getUserInfo(item.user_id)
+    if 'username' not in login_session or creator.id != login_session['user_id']:
+        return render_template('public_item.html', item=item)
+    else:
+        return render_template('private_item.html', item=item)
 
 
+# New item
+@app.route('/item/new', methods=['GET', 'POST'])
+def newItem():
+    if 'username' not in login_session:
+        return redirect('/login')
+    if request.method == 'POST':
+        newItem = Catalog(name=request.form['name'],
+                        description=request.form['description'],
+                        category=request.form['category'],
+                        user_id=login_session['user_id'])
+        session.add(newItem)
+        # flash('%s Successfully Created'% newItem.name)
+        session.commit()
+        return redirect(url_for('showMain'))
+    else:
+        return render_template('add.html')
+
+# Edit item
+
+@app.route('/item/<int:item_id>/edit', methods=['GET', 'POST'])
+def editItem(item_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    editedItem = session.query(Catalog).filter_by(id=item_id).one()
+    if login_session['user_id'] != editedItem.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to edit menu items to this restaurant. Please create your own restaurant in order to edit items.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        if request.form['description']:
+            editedItem.description = request.form['description']
+        if request.form['category']:
+            editedItem.category = request.form['category']
+        session.add(editedItem)
+        session.commit()
+        flash('Item Successfully Edited')
+        return redirect(url_for('showMain'))
+    else:
+        return render_template('edit.html', item=editedItem)
+
+# Delete an item
+@app.route('/item/<int:item_id>/delete', methods=['GET', 'POST'])
+def deleteItem(item_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    itemToDelete = session.query(Catalog).filter_by(id=item_id).one()
+    if login_session['user_id'] != itemToDelete.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to delete menu items to this restaurant. Please create your own restaurant in order to delete items.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        flash('Item Successfully Deleted')
+        return redirect(url_for('showMain'))
+    else:
+        return render_template('delete.html', item=itemToDelete)
+
+# JSON APIs to view all the catalog items
+@app.route('/catalog/json')
+def catalogJSON():
+    catalog = session.query(Catalog).all()
+    return jsonify(Catalog=[i.serialize for i in catalog])
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
